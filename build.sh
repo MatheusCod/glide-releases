231b98d4ed5bd291642ef615c32abbd57f0a47da
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-08 14:39:00 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -33,11 +33,11 @@ then
     sudo ./empacotar-rpm.sh glide glide-$github_version $github_version " " "Package Management for Go"
     if [[ $github_version > $ftp_version ]]
     then
-        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/glide/latest glide-$github_version"
-        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/glide/latest/glide-$ftp_version"
+        #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/glide/latest glide-$github_version"
+        #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/glide/latest/glide-$ftp_version"
         lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO1 $LOCALPATH/glide-$github_version-ppc64le.deb"
         sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O $REPO2 $ROOTPATH/glide-$github_version-1.ppc64le.rpm"
     fi
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/glide/ glide-$github_version"
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/glide/glide-$del_version" 
+    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/glide/ glide-$github_version"
+    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/glide/glide-$del_version" 
 fi
